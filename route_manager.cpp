49ec01c8b8f554e4e6eaf570fd759f5730d49c94
Igor S.K <garrik@navis.ru>
2015-12-11 12:02:29 +0300

    Сделаем наш DBus интрефейс рабочим, хоть и бесссысленным
    Как реализовать некоторый функционал экспортируемых нами
    DBus-методов? Через сгенерированный адаптер. А как его использовать?
    
    Дело в том, что в сгенерированном адаптере есть сгенерированные
    слоты и сигналы. Сейчас поговорим о слотах, т.к. они представляют
    наши DBus методы. Мы могли бы засунуть в эти сгенерированные
    слоты какие-то наши действия, но если мы решим расширить набор методов
    и  перегенерировать наш адаптер из xml, то все наши правки пропадут.
    Как же работать с этим адаптером, не трогая его руками?
    
    Для этого на помощь приходит система мета-типов/объектов QT. Для
    объекта типа сгенерированного адаптера должен быть задан объект родитель в
    терминах этой мета-системы, назовем его мета-родителем. Если у этого мета-
    родителя есть одноименные слоты с той же сигнатурой, что и слоты адаптера,
    то слоты мета-родителя будут автоматически вызваны из слотов объекта
    адаптера-мета-потомка. Так этот адаптер был сгенерирован.
    
    Этим и объясняется заморочка с классом-оберткой RouteManager, содержащим
    указатель на адаптер.
    
    Теперь на шине мы имеем наше приложени с одним объектом и, хоть и бессмысленно,
    но отвечающими методами.

--- route_manager.cpp
+++ route_manager.cpp
@@ -1,7 +1,16 @@
 #include "route_manager.h"
 
-
 RouteManager::RouteManager()
 {
     m_adaptor = new RouteManagerAdaptor(this);
+}
+
+QDBusObjectPath RouteManager::StartNav(double lat, double lon)
+{
+    return QDBusObjectPath("/routes/useless_route");
+}
+
+void RouteManager::StopNav(const QDBusObjectPath &route_path)
+{
+    return;
 }
--- route_manager.h
+++ route_manager.h
@@ -4,12 +4,17 @@
 #include "route_manager_adp.h"
 #include <QObject>
 #include <QDBusContext>
+#include <QDBusObjectPath>
 
 class RouteManager : public QObject
 {
     Q_OBJECT
 public:
     RouteManager();
+
+public Q_SLOTS:
+    QDBusObjectPath StartNav(double lat, double lon);
+    void StopNav(const QDBusObjectPath &route_path);
 
 private:
     RouteManagerAdaptor *m_adaptor;
